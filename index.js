4e38542bc6835edda749d8368c937d5748d80620
tisha-akter <tishaakter.jui79@gmail.com>
2023-06-11 03:04:50 +0600

    verified JWT token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -9,6 +10,24 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+  
+    // bearer token 
+    const token = authorization.split(' ')[1];
+  
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' })
+      }
+      req.decoded = decoded;
+      next();
+    })
+  
+  }
 
 
 
@@ -32,6 +51,18 @@ async function run() {
         const ClassesInfoCollection = client.db("summerCamp").collection("classesInfo");
         const usersCollection = client.db("summerCamp").collection("users");
         const selectedClassCollection = client.db("summerCamp").collection("selectedClasses");
+
+
+
+        // Token 
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+
+            res.send({ token })
+        })
+
+
 
         // classes and instruc related api 
         app.get('/classesInfo', async (req, res) => {
@@ -73,12 +104,12 @@ async function run() {
         })
 
         // for make instructor 
-        app.patch('/users/instructor /:id', async (req, res) => {
+        app.patch('/users/instructor/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
             const updateDoc = {
                 $set: {
-                    role: 'admin'
+                    role: 'instructor'
                 },
             };
             const result = await usersCollection.updateOne(filter, updateDoc);
