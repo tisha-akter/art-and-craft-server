3c4ef5d22dd02fbd2fa1d2a165dc4ce8442b48f4
tisha-akter <tishaakter.jui79@gmail.com>
2023-06-10 22:16:05 +0600

    added delete mathod in selectedClass related api to delete seclected class

--- index.js
+++ index.js
@@ -74,6 +74,13 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/selectedClasses/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await selectedClassCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
 
 
