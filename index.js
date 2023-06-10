860a1a149bdef61d832f109b7dd7796de5133916
tisha-akter <tishaakter.jui79@gmail.com>
2023-06-10 17:13:23 +0600

    added get method in selectedclass related apis to save card data

--- index.js
+++ index.js
@@ -17,60 +17,74 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    const ClassesInfoCollection = client.db("summerCamp").collection("classesInfo");
-    const usersCollection = client.db("summerCamp").collection("users");
-    const selectedClassCollection = client.db("summerCamp").collection("selectedClasses");
+        const ClassesInfoCollection = client.db("summerCamp").collection("classesInfo");
+        const usersCollection = client.db("summerCamp").collection("users");
+        const selectedClassCollection = client.db("summerCamp").collection("selectedClasses");
 
-    // classes and instruc related api 
-    app.get('/classesInfo', async(req, res) => {
-        const result = await ClassesInfoCollection.find().toArray();
-        res.send(result);
-    })
+        // classes and instruc related api 
+        app.get('/classesInfo', async (req, res) => {
+            const result = await ClassesInfoCollection.find().toArray();
+            res.send(result);
+        })
 
-    // users related api 
-    app.post('/users', async (req, res) => {
-        console.log(user)
-        const query = { email: user.email }
-        const existingUser = await usersCollection.findOne(query);
-       
-        if (existingUser) {
-          return res.send({ message: 'user already exists' })
-        }
-        const result = await usersCollection.insertOne(user);
-        res.send(result);
-      });
+        // users related api 
+        app.post('/users', async (req, res) => {
+            console.log(user)
+            const query = { email: user.email }
+            const existingUser = await usersCollection.findOne(query);
 
-
-    //   selectedClass related apis 
-    app.post('/selectedClasses', async(req, res) => {
-        const item = req.body;
-        console.log(item);
-        const result = await selectedClassCollection.insertOne(item)
-        res.send(result);
-    })
+            if (existingUser) {
+                return res.send({ message: 'user already exists' })
+            }
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
 
 
+        //   selectedClass related apis
+        app.get('/selectedClasses', async (req, res) => {
+            const email = req.query.email;
+            if (!email) {
+                res.send([]);
+            }
+
+            const query = { email: email };
+            const result = await selectedClassCollection.find(query).toArray();
+            res.send(result);
+
+        });
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        app.post('/selectedClasses', async (req, res) => {
+            const item = req.body;
+            console.log(item);
+            const result = await selectedClassCollection.insertOne(item)
+            res.send(result);
+        })
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
