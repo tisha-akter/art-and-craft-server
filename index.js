b1e32a956f8318b067597899b9292961467d05b1
tisha-akter <tishaakter.jui79@gmail.com>
2023-06-11 00:38:44 +0600

    added get method in user related api to get get user data

--- index.js
+++ index.js
@@ -40,9 +40,14 @@ async function run() {
         })
 
         // users related api 
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result);
+          });
+
+
         app.post('/users', async (req, res) => {
             const user = req.body;
-            console.log(user)
             const query = { email: user?.email }
             const existingUser = await usersCollection.findOne(query);
 
